aab38b62c16d7f7b5e2284a2fdea8fdc2ae065f5
yucheng <yucheng881111@gmail.com>
2022-11-11 12:18:07 +0800

    add eval_value

--- train/evaluate_model.cpp
+++ train/evaluate_model.cpp
@@ -2,6 +2,8 @@
 #include <torch/torch.h>
 #include "neural/network.h"
 
+#define EVAL_VALUE
+
 std::vector<std::vector<std::vector<std::vector<int>>>> replay_buffer;
 std::vector<std::vector<int>> replay_buffer_policy;
 std::vector<int> is_win;
@@ -110,9 +112,85 @@ int main() {
 		}
 	}
 	*/
+
+	int len = replay_buffer[0].size();
+	std::cout << "data index 0 has size: " << len << std::endl;
+
+	#ifdef EVAL_VALUE
+		for (int trajectory_idx = 2; trajectory_idx <= len-2; ++trajectory_idx) {
+			int k = 0;
+			std::cout << "trajectory idx: " << trajectory_idx << std::endl;
+			for (int j = trajectory_idx; j >= trajectory_idx - (seq_len - 1); --j) {
+				//std::cout << replay_buffer[0][j] << std::endl;
+				for (int m = 0; m < 9; ++m) {
+					for (int n = 0; n < 9; ++n) {
+						if (replay_buffer[0][j][m][n] == 1) {
+							tmp_data.index_put_({0, k, m, n}, 1);
+						} else if (replay_buffer[0][j][m][n] == -1) {
+							tmp_data.index_put_({0, k + seq_len, m, n}, -1);
+						}
+					}
+				}
+				k++;
+			}
+
+			int p = replay_buffer_policy[0][trajectory_idx];   // policy target
+			int v = is_win[0];                                 // value target
+
+			if (p < 81) {  // current play black
+				// last channel (play black => all 1)
+				for (int m = 0; m < 9; ++m) {
+					for (int n = 0; n < 9; ++n) {
+						tmp_data.index_put_({0, channel_size - 1, m, n}, 1);
+					}
+				}
+				
+				tmp_p_label.index_put_({0, p}, 1);
+				if (v == 1) {
+					tmp_v_label.index_put_({0}, 1);   // now play black and black win
+				} else {
+					tmp_v_label.index_put_({0}, -1);  // now play black and white win
+				}
+				
+			} else {       // current play white
+				// last channel (play white => all -1)
+				for (int m = 0; m < 9; ++m) {
+					for (int n = 0; n < 9; ++n) {
+						tmp_data.index_put_({0, channel_size - 1, m, n}, -1);
+					}
+				}
+				
+				tmp_p_label.index_put_({0, p - 81}, 1);
+				if (v == -1) {
+					tmp_v_label.index_put_({0}, 1);   // now play white and white win
+				} else {
+					tmp_v_label.index_put_({0}, -1);  // now play white and black win
+				}
+				
+			}
+
+
+			int channel_size = 7;
+			torch::Device device = torch::cuda::is_available() ? torch::kCUDA : torch::kCPU;
+			// planes, height, width, filters, num_res_blocks, policy_size
+			const auto net_op = az::NetworkOptions{channel_size, 9, 9, 128, 2, 81};
+			az::AlphaZeroNetwork net(net_op);
+			torch::load(net, "epoch150_weights.pt");
+			net->to(device);
+			tmp_data = tmp_data.to(device);
+			net->eval();
+			auto [v_out, p_out] = net->forward(tmp_data);
+			std::cout << tmp_v_label << "  ";
+			float v_pred = v_out.view(-1)[0].template item<float>();
+			std::cout << "value: " << v_pred << std::endl;
+		}
+
+	#endif
+
+
+
 	int k = 0;
 	int trajectory_idx = 2;
-	std::cout << "data index 0 has size: " << replay_buffer[0].size() << std::endl;
 	std::cout << "input trajectory index: ";
 	std::cin >> trajectory_idx;
 	for (int j = trajectory_idx; j >= trajectory_idx - (seq_len - 1); --j) {
